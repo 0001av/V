7ea598773de10f30d2eb0847238b797fc38647e6
0001av <vanshkulshrestha@gmail.com>
2026-02-03 17:34:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      background:#ff758c;
+      color:white;
+      font-size:32px;
+      font-family:Arial;
+      text-align:center;
+    ">
+      💖 Yayyy! I knew it 😍<br>Happy Valentine’s Day 💘
+    </div>
+  `;
 });
